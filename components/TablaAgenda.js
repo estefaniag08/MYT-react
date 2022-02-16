6315539d6a8e31e4653d51efc5b1b3a35db490b8
estefaniag08 <estefaniag08@hotmail.com>
2022-02-16 12:43:22 -0500

    Cambios por la instalación de firebase y detalls en la tabla de agenda

--- components/TablaAgenda.js
+++ components/TablaAgenda.js
@@ -1,4 +1,5 @@
-
+import Columna from "./Tablas/Columna";
+import styles from "../styles/TablaAgenda.module.css";
 function TablaAgenda(){
   return (
     <div className="bg-gray-500 lg:flex lg:w-1/2 grid gap-3 md:m-3">
