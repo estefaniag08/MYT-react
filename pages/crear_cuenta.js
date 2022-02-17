29b4a92b2f0c735e10b0b952b4fc0093ab19f29b
MoraSebastian <morasabogalsebastian@gmail.com>
2022-02-17 13:13:04 -0500

    Añadiendo la fuente a la página de creación de cuenta

--- pages/crear_cuenta.js
+++ pages/crear_cuenta.js
@@ -1,11 +1,22 @@
+import Head from "next/head";
 import SimpleLayout from "../components/SimpleLayout";
 import CrearCuentaContenedor from "../components/CrearCuentaContenedor";
 
 function CrearCuenta() {
   return (
-   <SimpleLayout>
-     <CrearCuentaContenedor />
-   </SimpleLayout>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head> 
+      <SimpleLayout>        
+        <CrearCuentaContenedor />          
+      </SimpleLayout>
+    </>
   )
 }
 
