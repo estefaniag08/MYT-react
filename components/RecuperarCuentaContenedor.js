a5de4765ac7d89d7bd125e1e80ed2c2c3f833baf
MoraSebastian <morasabogalsebastian@gmail.com>
2022-02-17 13:06:28 -0500

    Añadido contenido de componente de recuperacion de cuenta

--- components/RecuperarCuentaContenedor.js
+++ components/RecuperarCuentaContenedor.js
@@ -1,9 +1,14 @@
 function RecuperarCuentaContenedor() {
   return (
-    <div className="bg-white shadow-md rounded-md opacity-80 w-full h-full">
-      <h1>Recuperar cuenta</h1>
+    <div className="bg-white shadow-md rounded-md opacity-80 w-full h-full flex flex-col justify-center items-center">
+      <h1 className="bg-color-blue-200 text-6xl font-semibold">Recupera tu cuenta</h1>
+      <h1 className="text-2xl py-4">Se enviará un enlace de recuperación de la cuenta a su correo.</h1>      
+      <h1 className="text-5xl pt-4">correo electrónico:</h1>
+      <input type="text" id="correo" name="correo" className="bg-white border-4 border-gray-600"/>      
+      <button type="button" className="bg-color-blue-200 border-4">recuperar cuenta</button>      
     </div>
   )
 }
 
 export default RecuperarCuentaContenedor
+
