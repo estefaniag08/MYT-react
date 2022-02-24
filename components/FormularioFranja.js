20e6e396381102b6bbe2e59cc3afe9f99c4b43f7
MoraSebastian <morasabogalsebastian@gmail.com>
2022-02-24 14:49:41 -0500

    Dejando listo el responsive del formulario de adición de franja

--- components/FormularioFranja.js
+++ components/FormularioFranja.js
@@ -1,50 +1,42 @@
 import styles from "../styles/FormFranja.module.css"
 
 function FormularioFranja() {
-  return (
-    <div class="modal_crearfranja" className={`${styles.modal_crearfranja}`}>
+  return (                    
+    <div class="contenido_franja" className={`${styles.contenido_franja} font-bold grid grid-cols-1 text-center`}>
+      
+      <h1 className={`${styles.h1} text-center text-4xl col-span-4 inset-1/2`}> información general franja</h1>
+      
+      <div className=" text-black grid grid-cols-2 gap-0 items-center text-2xl sm:grid-cols-4 sm:gap-3 sm:text-3xl">
+        <h3 className={`${styles.p} `}> nombre:</h3>            
+        <input type="text" className=" bg-white  text-xl h-6 w-24 sm:h-8" />          
+        <h3 className="">tipo:</h3>
+        <input type="text" className="bg-white  text-xl h-6 w-24 sm:h-8" />
+        <h3>descripción:</h3>
+        <input type="text" className="bg-white  text-xl h-6 w-24 sm:h-8" />              
+      </div>
+                    
+      <h1 className={`${styles.h1} pt-5 text-4xl col-span-4 inset-1/2`}> frecuencia</h1>
+      
+      <div className="gap-3 flex items-center justify-center text-black text-2xl font-bold">
+        <button className="text-2xl font-bold bg-white w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-[#49D1CD] focus:text-white"> D </button>
+        <button className="text-2xl font-bold bg-white w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-[#49D1CD] focus:text-white"> l </button>
+        <button className="text-2xl font-bold bg-white w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-[#49D1CD] focus:text-white"> m </button>
+        <button className="text-2xl font-bold bg-white w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-[#49D1CD] focus:text-white"> m </button>
+        <button className="text-2xl font-bold bg-white w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-[#49D1CD] focus:text-white"> j </button>
+        <button className="text-2xl font-bold bg-white w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-[#49D1CD] focus:text-white"> v </button>
+        <button className="text-2xl font-bold bg-white w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-[#49D1CD] focus:text-white"> s </button>
+      </div>
 
-        <div class="contenedor_franja" className={`${styles.contenedor_franja} flex flex-col items-center`}>            
-          <header className={`${styles.header}`}>añadir franja</header>
-          <label for="btn_crearfranja" >X</label>
-            
-          <div class="contenido_franja" className={`${styles.contenido_franja} flex flex-wrap flex-col justify-center items-center`}>
-            
-            <h1 className={`${styles.h1} col-span-4 inset-1/2`}> información general</h1>
-            
-            <div className="columns-4 grid grid-cols-4 flex flex-wrap items-center">
-              <h3 className={`${styles.p}  text-right`}> nombre:</h3>            
-              <input type="text" className=" bg-white  h-8 w-32 text-xl h-8 w-24" />          
-              <h3 className="">tipo:</h3>
-              <input type="text" className="bg-white  h-8 w-32 text-xl h-8 w-24" />
-              <h3>descripción:</h3>
-              <input type="text" className="bg-white  h-8 w-32 text-xl h-8 w-24" />              
-            </div>
-                          
-            <h1 className={`${styles.h1} col-span-4 inset-1/2`}> frecuencia</h1>
-            
-            <div className="gap-3 flex items-center justify-center">
-              <button className="text-2xl bg-slate-300 w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-sky-600"> D </button>
-              <button className="text-2xl bg-slate-300 w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-sky-600"> l </button>
-              <button className="text-2xl bg-slate-300 w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-sky-600"> m </button>
-              <button className="text-2xl bg-slate-300 w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-sky-600"> m </button>
-              <button className="text-2xl bg-slate-300 w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-sky-600"> j </button>
-              <button className="text-2xl bg-slate-300 w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-sky-600"> v </button>
-              <button className="text-2xl bg-slate-300 w-12 h-12 rounded-full hover:bg-sky-300 focus:bg-sky-600"> s </button>
-            </div>
+      <h1 className={`${styles.h1} pt-5 text-4xl col-span-4 inset-1/2`}> Rango</h1>
 
-            <h1 className={`${styles.h1} col-span-4 inset-1/2`}> Rango</h1>
-
-            <div className="grid grid-cols-4 gap-4  items-center">
-              <h3 className="text-right ">hora inicio:</h3>
-              <input type="text" className="bg-white  h-8 w-32 text-xl h-8 w-24 " />
-              <h3 className="text-right ">hora final:</h3>
-              <input type="text" className="bg-white  h-8 w-32 text-xl h-8 w-24 " />
-            </div>            
-          </div>          
-          <button type="button" className="bg-white font-bold text-4xl p-1 w-24 mt-[470px]">añadir</button>                    
-        </div>        
-      </div>    
+      <div className="text-black grid grid-cols-2 gap-0 items-center text-2xl sm:grid-cols-4 sm:gap-3 sm:text-3xl">
+        <h3 className="text-right ">hora inicio:</h3>
+        <input type="text" className="bg-white text-xl h-6 w-24 sm:h-8" />
+        <h3 className="text-right ">hora final:</h3>
+        <input type="text" className="bg-white text-xl h-6 w-24 sm:h-8" />
+      </div>            
+    </div>          
+                   
   )
 }
 
