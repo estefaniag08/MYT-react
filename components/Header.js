ba8cf524fe86ff1659eec292326fd3a308e58c08
MoraSebastian <morasabogalsebastian@gmail.com>
2022-03-03 14:32:45 -0500

    Inclusión de la barra lateral para ser desplegada desde el header

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,26 @@
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
+import styles_side from "../styles/Sidebar.module.css"
+import Sidebar from "./Sidebar";
 
 function Header() {
   return (
     <header className={`${styles.header} `}>
-      <div className="flex items-center justify-center h-full">
-        <div className="w-1/12 pl-4 hover:cursor-pointer">
-          <img
-            layout="fixed"
-            src="/img/hamburguer.svg"
-            width={40}
-            height={40}
-          />{" "}
-        </div>
+      <div className="flex items-center justify-center h-full">      
+
+        <input type="checkbox" id="btn_sidebar" className={`${styles_side.btn_sidebar} ${styles_side.botonIcono}`} />
+        <label for="btn_sidebar" class="lbl_crearfranja" className={`z-[2000] text-4xl p-2 ${styles_side.lbl_crearfranja} ${styles_side.boton} `}> 
+        
+          <div className="w-13 h-13 z-[2000] pl-2 hover:cursor-pointer ">
+            <img
+              layout="responsive"
+              src="/img/hamburguer.svg"
+              
+            />{" "}
+          </div>
+        </label>
+        <Sidebar/>
+
         <Link href="/horario">
           <div className=" flex items-center justify-center w-11/12 md:text-8xl sm:text-6xl text-5xl text-center text-white font-black">
             <a className="hover:cursor-pointer flex">
