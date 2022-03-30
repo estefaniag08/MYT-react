acc23022de3436a95fc014b9c3aba477bf97eca6
MoraSebastian <morasabogalsebastian@gmail.com>
2022-03-30 15:19:34 -0500

    Añadido apartado de accesibilidad en formulario de inicio de sesión

--- components/IniciarSesion.js
+++ components/IniciarSesion.js
@@ -35,11 +35,8 @@ function IniciarSesion(){
         array.push(doc.id)
       })
       //alert("usuarios con el alias: " + array.length) 
-      if (array.length == 1){
-        alert("iniciando sesión ....")
-        alert("id usuario que se guarda en el localstorage: " + array[0])
-        localStorage.setItem("IdUser", array[0]);
-        alert("recuperando id del localstorage: " + localStorage.getItem("IdUser"))
+      if (array.length == 1){                
+        localStorage.setItem("IdUser", array[0]);        
         router.push("/horario")
       } else {
         alert("el usuario no existe, si desea puede registrarlo ...")
@@ -69,7 +66,8 @@ function IniciarSesion(){
                   <TextField                     
                     type="text"
                     name="username"
-                    id="username" 
+                    id="username"
+                    aria-label="campo de nombre de usuario"
                     className=" w-full h-9 "                                                        
                   />                   
 
@@ -77,7 +75,8 @@ function IniciarSesion(){
                   <TextField                    
                     type="password" 
                     name="password"
-                    id="password"   
+                    id="password"
+                    aria-label="campo de contraseña"   
                     className=" h-9 w-full "                 
                   />                   
 
