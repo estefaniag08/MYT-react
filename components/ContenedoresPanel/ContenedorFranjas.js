ccce853133c78958162fb59a3575e29b9a057417
MoraSebastian <morasabogalsebastian@gmail.com>
2022-04-05 15:20:32 -0500

    Eliminación de franjas con confirmación

--- components/ContenedoresPanel/ContenedorFranjas.js
+++ components/ContenedoresPanel/ContenedorFranjas.js
@@ -2,7 +2,8 @@ import styles from "../../styles/ContenedoresPanel/ContenedorTareasFranjas.modul
 import FormModificarFranja from "../FormModificarFranja";
 import styles_franja from "../../styles/ModalCrearFranja.module.css";
 import styles_modfranja from "../../styles/ModalModFranja.module.css";
-
+import { collection, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
+import { firestore } from "../../firebase/clientApp";
 import FormCrearFranja from "../FormCrearFranja";
 
 function ContenedorFranjas({ setFranja, idFranja }) {
@@ -41,6 +42,43 @@ function ContenedorFranjas({ setFranja, idFranja }) {
       document.getElementById("btn_modfranja").checked = false;
     }
   };
+  const eliminarFranja = async () => {      
+    if (idFranja) {   
+      var opcion = confirm("Estás seguro de que desea eliminar la franja seleccionada ? ")
+      alert("id franja : " + idFranja)  
+      if (opcion === true){
+        const usuario = localStorage.getItem("IdUser");         
+        const franjaEditar = doc(
+          firestore,
+          `franjas/${usuario}/franja`,
+          idFranja
+        );            
+        const franjaEditarDatos = await getDoc(franjaEditar);
+        const franja = {
+          activo: false,
+          descripcion: franjaEditarDatos.data().descripcion,
+          frecuencia: franjaEditarDatos.data().frecuencia,
+          hora_final: franjaEditarDatos.data().hora_final,
+          hora_inicio: franjaEditarDatos.data().hora_inicio,        
+          nombre: franjaEditarDatos.data().nombre,
+          tipo: franjaEditarDatos.data().tipo                                
+        }                                                                 
+        try {
+          const franjaUsuarioId = doc(
+            firestore,
+            `franjas/${usuario}/franja`,
+            idFranja
+          );
+          await updateDoc(franjaUsuarioId, franja);
+          alert("despues de eliminar franja ...")
+        } catch (error) {
+          console.error(error);
+        }
+      }      
+    } else {
+      alert("No se ha seleccionado una franja para eliminar.")
+    }
+  }
   return (
     <div
       className={`flex-col gap-3 justify-center items-center ${styles.contenedor}`}
@@ -93,7 +131,7 @@ function ContenedorFranjas({ setFranja, idFranja }) {
         </label>
         <FormModificarFranja idFranja={idFranja} />
 
-        <button className={`text-4xl p-2 ${styles.boton}`}>
+        <button onClick={eliminarFranja} className={`text-4xl p-2 ${styles.boton}`}>
           <div className="w-8 h-8">
             <img
               src="/img/calendar/calendar-remove.png"
