4e9086f8a573f2cd6082556160f1c336bb0d1cb1
MoraSebastian <morasabogalsebastian@gmail.com>
2022-04-05 00:47:28 -0500

    Mostrando las tareas en su panel de horario

--- components/ContenedoresPanel/ContenedorImportantes.js
+++ components/ContenedoresPanel/ContenedorImportantes.js
@@ -1,16 +1,53 @@
 import styles from "../../styles/ContenedoresPanel/ContenedorImportantes.module.css"
-function ContenedorImportantes() {
+import { cambiarDia, esHoy, obtenerNombreDelDiaYNumero, obtenerTituloDia } from '../../services/date.service';
+import { useEffect, useState } from "react";
+import { getTime } from "../../services/date.service";
+
+function ContenedorImportantes({ listaTareas, fecha, setFecha, setTareaSeleccionada }) {
+  const [tituloDia, setTituloDia] = useState(obtenerTituloDia(fecha));
+  const clicSiguienteDia = () => {
+    cambiarDia(1, fecha, setFecha, setTituloDia);
+  }
+  const clicDiaAnterior = () => {
+    cambiarDia(-1, fecha, setFecha, setTituloDia);
+  }
+  //alert("lista tareas cantidad: " + listaTareas.length)
+  /*alert("lista tareas nombre tarea 1: " + listaTareas[0].nombre)
+  alert("lista tareas nombre tarea 2: " + listaTareas[1].nombre)
+  alert("lista tareas nombre tarea 3: " + listaTareas[2].nombre)
+  */
+  
+  let tareas = []
+  let muchastareas = ""
+  if (listaTareas.length > 0){
+    for (let i = 0; i < 3; i++){      
+      const tarea = listaTareas[i] ? listaTareas[i].nombre + " - " + getTime(listaTareas[i].horaEntrega) : " - "      
+      tareas.push(tarea)
+    }
+    if (listaTareas.length > 3){
+      muchastareas = " ... "
+    }
+  }else {
+    for (let i = 0; i < 3; i++){
+      tareas.push(" - ")
+    }
+  }
+
   return (
     <div className={styles.contenedor}>
         <div className="text-5xl flex gap-4 items-center justify-center p-1 font-bold">
-            <button type="button" className="font-bold">{"<"}</button>
-            <h1 className="lg:text-5xl text-3xl">TAREAS ## NOV</h1>
-            <button type="button" className="font-bold">{">"}</button>
+            <button onClick={clicDiaAnterior} type="button" className="font-bold">
+              {"<"}
+            </button>
+            <h1 className="lg:text-5xl text-3xl">TAREAS {tituloDia}</h1>
+            <button onClick={clicSiguienteDia} type="button" className="font-bold">
+              {">"}
+            </button>
         </div>
-        <div className="text-2xl md:text-4xl grid items-center justify-center pb-4 whitespace-normal p-2">
-            <h2>Tarea 1 - 10 AM</h2>
-            <h2>Tarea 2 - 10 AM</h2>
-            <h2>Tarea 3 - 10 AM</h2>
+        <div className="text-center text-2xl md:text-4xl grid items-center justify-center pb-4 whitespace-normal p-2">
+          <h2>{`${tareas[0]}`}</h2>
+          <h2>{`${tareas[1]}`}</h2>
+          <h2>{`${tareas[2]} ${muchastareas}`}</h2>            
         </div>
     </div>
   )
--- components/PanelHorario.js
+++ components/PanelHorario.js
@@ -1,12 +1,45 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/PanelHorario.module.css";
 import ContenedorFranjas from "./ContenedoresPanel/ContenedorFranjas";
 import ContenedorImportantes from "./ContenedoresPanel/ContenedorImportantes";
 import ContenedorTareas from "./ContenedoresPanel/ContenedorTareas";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../firebase/clientApp";
+import { obtenerSoloActivas } from "../services/docList.service";
 
 function PanelHorario({setFranja, idFranja}) {
   const [prueba, setPrueba] = useState("");
+
+  const [listaTareas, setListaTareas] = useState([]);
+  const [listaFranjas, setListaFranjas] = useState([]);
+  const [tareaSeleccionada, setTareaSeleccionada] = useState();
+  const [fecha, setFecha] = useState(new Date());
+  const usuario = "vMCIp2NBOORMJhVcw9HV"; //Como prueba
+
+  useEffect(() => {
+    const fechaInicio = new Date(fecha);
+    fechaInicio.setHours(0, 0, 0, 0)
+    const fechaFinal = new Date();
+    fechaFinal.setDate(fecha.getDate() + 1);
+    fechaFinal.setHours(0, 0, 0, 0);
+    llenarListaTareas(fechaInicio, fechaFinal);
+  }, [fecha]);
+
+  const llenarListaTareas = async (fechaInicio, fechaFinal) => {
+    const coleccionTareas = collection(firestore, `tareas/${usuario}/tarea`);
+    const queryTareas = query(
+      coleccionTareas,
+      where("fecha_entrega", ">=", fechaInicio),
+      where("fecha_entrega", "<", fechaFinal),
+    );
+    let arregloTareas = []
+    const snapshotTareas = await getDocs(queryTareas);
+    arregloTareas = obtenerSoloActivas(snapshotTareas);
+    setListaTareas(arregloTareas);
+    const nuevaTareaSeleccionada = arregloTareas[0]
+    setTareaSeleccionada(nuevaTareaSeleccionada);
+  }
   return (
     <div className="2xl:w-2/12 lg:self-center grid gap-3  lg:mt-3">
       <div>
@@ -24,7 +57,7 @@ function PanelHorario({setFranja, idFranja}) {
           </button>
         </Link>
       </div>
-      <ContenedorImportantes />
+      <ContenedorImportantes listaTareas={listaTareas} fecha={fecha} setFecha={setFecha} setTareaSeleccionada={setTareaSeleccionada}  />
       <ContenedorFranjas setFranja={setFranja} idFranja={idFranja}/>
       
       <input type="text" id="pruebatxt" onChange={(e) => {setPrueba(e.target.value)}}/>
